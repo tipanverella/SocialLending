d5d83f3ad8530be5450f316cc2dfc229b254e5ca
J. Tipan Verella <tipan.verella@gmail.com>
2014-08-04 18:55:11 -0400

    working on the GetKiva routine

--- src/kiva.py
+++ src/kiva.py
@@ -13,5 +13,21 @@ import pandas as pd
 
 from collections import deque, Counter
 
+SLrootDirectory = os.path.realpath('.')
+
+def GetKiva(getnew=False):
+	if getnew:
+		print "Getting a new JSON Snapshot!"	
+		print "I will put it above ", SLrootDirectory
+		kivapath = os.path.realpath('..')+"/kiva_ds_json.zip"
+
+		if not os.path.exists(kivapath):
+			print "You don't seem to have a snapshot.  I will get one from the kiva website!"
+			os.system("wget http://s3.kiva.org/snapshots/kiva_ds_json.zip --output-document="+kivapath)
+		else:
+			os.system("ls -lstrh "+kivapath)
+	else:
+		pass
 
 
+GetKiva(True)
